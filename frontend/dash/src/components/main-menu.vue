207926dced371b27b4cd5fadee62e929b298be67
Yusuf Ozturk <yusuf.ozturk3@hva.nl>
2026-02-17 22:46:08 +0100

    starting to lean vue

--- frontend/dash/src/components/main-menu.vue
+++ frontend/dash/src/components/main-menu.vue
@@ -4,11 +4,37 @@
 </script>
 
 <style scoped>
+  body{
+    margin: 0;
+  }
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
 
+  .header-left {
+    color: var(--text-color);
+    font-weight: bold;
+    font-size: 2rem;
+
+  }
 </style>
 
 
 <template>
-
+  <body>
+<div class="header">
+  <div class="header-left">
+    <p>Stock Creeper</p>
+  </div>
+  <div class="header-right">
+    <div class="search">
+      <input class="search-input" type="text" placeholder="Search Stock" />
+    </div>
+  </div>
+</div>
+  </body>
 </template>
 
