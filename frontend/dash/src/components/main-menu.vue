39896dda04c328dc40eb127ca2d102a49095ebb9
Yusuf Ozturk <yusuf.ozturk3@hva.nl>
2026-02-18 17:37:14 +0100

    made the front and backend structure

--- frontend/dash/src/components/main-menu.vue
+++ frontend/dash/src/components/main-menu.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import searchIcon from '../assets/searchicon.png'
 
 
 </script>
@@ -11,15 +12,37 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: row;
+    flex-direction: column;
   }
 
   .header-left {
     color: var(--text-color);
     font-weight: bold;
     font-size: 2rem;
-
+    padding: 0 10px;
   }
+
+  .search-input {
+    width: 19rem;
+    height: 2rem;
+    outline: none;
+    border: none;
+    border-radius: 0.2rem;
+    padding: 0.2rem 0.5rem;
+  }
+  .search {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .search-icon {
+    width: 1.5rem;
+    position: absolute;
+    padding: 5px 5px;
+    cursor: pointer;
+  }
+
 </style>
 
 
@@ -31,6 +54,7 @@
   </div>
   <div class="header-right">
     <div class="search">
+      <img :src="searchIcon" alt="search icon" class="search-icon" />
       <input class="search-input" type="text" placeholder="Search Stock" />
     </div>
   </div>
---
+++ src/main/java/stock/controller/StockController.java
@@ -0,0 +1,4 @@
+package stock.controller;
+
+public class StockController {
+}
